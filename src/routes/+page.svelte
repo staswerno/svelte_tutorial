d0eb22d1dc8c1466d30e62491b8ac479a9bc55d3
staswerno <staswerno@gmail.com>
2024-01-10 17:57:01 +0100

    add image component element and dynamic attribute

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-    let name = 'stashi';
+    let name = 'THREE BODY PROBLEM';
+    let src = 'https://media.gq-magazine.co.uk/photos/63bee87a57e25ad39c962d73/16:9/w_1280,c_limit/The-three-body-problem-hp.jpg';
 </script>
 
-<h1>hello {name.toUpperCase()}</h1>
+<h1>{name.toLowerCase()} themed svelte tutorial</h1>
+<img src={src} alt="{name.toLowerCase()} artwork 1">
+<!-- if the src variable is src, this can be written as: -->
+<img {src} alt="{name.toLowerCase()} artwork 2">
