0db000d4aa65380bb6b2ed7c679b925076dc2d77
staswerno <staswerno@gmail.com>
2024-01-11 15:53:29 +0100

    add nested component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     let name = 'THREE BODY PROBLEM';
     let src = 'https://media.gq-magazine.co.uk/photos/63bee87a57e25ad39c962d73/16:9/w_1280,c_limit/The-three-body-problem-hp.jpg';
+    import Nested from "./Nested.svelte";
 </script>
 
 <h1>{name.toLowerCase()} themed svelte tutorial</h1>
@@ -9,12 +10,21 @@
 <!-- svelte will give a11y warning in case of no alt -->
 <img src={src} alt="{name.toLowerCase()} artwork 1">
 
-<!-- if the src variable is src, this can be written in shorthand: -->
+<!-- if the src variable name is src, this can be written in shorthand: -->
 <img {src} alt="{name.toLowerCase()} artwork 2">
+
+<p>this content is from this component</p>
+
+<!-- always capitalise component names to distinguish from html elements -->
+<Nested />
 
 <!-- styling is scoped to the component -->
 <style>
     h1 {
+        color: midnightblue;
+        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+    p {
         color: midnightblue;
         font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     }
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,18 @@
+<div class="nested-component-container">
+    <div>
+        <p>this content is from a <i>nested</i> component</p>
+    </div>
+    <div>
+        <p>notice that the styles from <code>+page.svelte</code> don't apply here</p>
+    </div>
+</div>
+
+<style>
+    .nested-component-container {
+        border: 3px solid midnightblue;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }                       
+</style>
