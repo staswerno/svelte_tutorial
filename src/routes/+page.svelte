6bca7c5d1c26a635621f5cf2663d4cb00ef8c6fb
staswerno <staswerno@gmail.com>
2024-01-11 16:09:04 +0100

    add html tag test input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     let name = 'THREE BODY PROBLEM';
     let src = 'https://media.gq-magazine.co.uk/photos/63bee87a57e25ad39c962d73/16:9/w_1280,c_limit/The-three-body-problem-hp.jpg';
     let string = 'this string contains <strong>html</strong> tags';
+    let htmlTestInput = '<input type="text" value="this is a test">'
 </script>
 
 <h1>{name.toLowerCase()} themed svelte tutorial</h1>
@@ -20,6 +21,7 @@
 <Nested />
 
 <p>{@html string}</p>
+{@html htmlTestInput}
 
 <!-- styling is scoped to the component -->
 <style>
