dd23eb232c5390ff7b5c57d5a5f19f8592ce05e1
staswerno <staswerno@gmail.com>
2024-01-12 17:47:53 +0100

    add reactive variables, declarations, statements

--- src/routes/reactivity/+page.svelte
+++ src/routes/reactivity/+page.svelte
@@ -1,1 +1,25 @@
-<p>laksnf</p>
+<script>
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	} 
+
+    $: doubled = count * 2;
+
+    $: {
+        console.log(`count is ${count}`);
+        console.log(`doubled is ${doubled}`);
+    }
+
+    $: if (count >= 10) {
+        console.log(`kitten death count is dangerously high you monster!`);
+    }
+</script>
+
+<button on:click={increment}>
+	# kittens killed at your behest: {count}
+	{count === 1 ? 'kitten' : 'kittens'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
