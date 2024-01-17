4e94d17f2477fdb9594fb3c0f82580e19f2ad1b4
staswerno <staswerno@gmail.com>
2024-01-17 17:06:02 +0100

    add assignment to property

--- src/routes/reactivity/+page.svelte
+++ src/routes/reactivity/+page.svelte
@@ -23,22 +23,26 @@
     function addNumber() {
         // svelte's reactivity is triggered by assignment
         // so this alone won't trigger a re-render
-        numbers.push(numbers.length + 1);
+        // numbers.push(numbers.length + 1);
 
         // you can get around it by using a redundant assignment:
-        numbers = numbers;
+        // numbers = numbers;
 
         // or, using the spread operator:
         // numbers = [...numbers, numbers.length + 1];
 
         // also works for pop, shift, unshift, splice, etc.
+
         // Assignments to properties of arrays and objects
         // e.g. obj.foo += 1 or array[i] = x
-        // work the same way as assignments to the values themselves.
-
+        // work the same way as assignments to the values themselves:
+        // numbers[numbers.length] = numbers.length + 1;
+        // note that this method works because the length of the array
+        // will always be one more than the index of the last element
+     
         // RULE: updated variable name must appear on the left
         // of the assignment operator
-        
+
     }
 
     $: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
