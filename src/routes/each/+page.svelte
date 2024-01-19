673460dc25cfb90092576cc5d639ca8cde75203b
staswerno <staswerno@gmail.com>
2024-01-19 16:59:07 +0100

    add each loop

---
+++ src/routes/each/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+	let selected = colors[0];
+</script>
+
+<h1 style="color: {selected}">Pick a colour</h1>
+
+<div>
+    <!-- rather than write a button for each color... -->
+    <!-- get current index as second argument -->
+    {#each colors as color, i}
+        <button
+            aria-current={selected === color}
+            aria-label="{color}"
+            style="background: {color}"
+            on:click={() => selected = color}
+        >index = {i}</button>
+    {/each}
+    <!-- The expression (colors, in this case) can be any array
+         or array-like object (i.e. it has a length property). 
+         You can loop over generic iterables with each [...iterable].-->
+</div>
+
+<style>
+    * {
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+	h1 {
+		transition: color 0.2s;
+	}
+
+	div {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		grid-gap: 5px;
+		max-width: 400px;
+	}
+
+	button {
+		aspect-ratio: 1;
+		border-radius: 50%;
+		background: var(--color, #fff);
+		transform: translate(-2px,-2px);
+		filter: drop-shadow(2px 2px 3px rgba(0,0,0,0.2));
+		transition: all 0.1s;
+	}
+
+	button[aria-current="true"] {
+		transform: none;
+		filter: none;
+		box-shadow: inset 3px 3px 4px rgba(0,0,0,0.2);
+	}
+</style>
