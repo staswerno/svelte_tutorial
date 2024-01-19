7483ee77961ccf1e319b8c74cf0e2c440aa44231
staswerno <staswerno@gmail.com>
2024-01-19 15:48:49 +0100

    add props info

--- src/routes/props/+page.svelte
+++ src/routes/props/+page.svelte
@@ -1,5 +1,26 @@
 <script>
 	import Nested from './Nested.svelte';
+    import PackageInfo from './PackageInfo.svelte';
+
+	const pkg = {
+		name: 'svelte',
+		speed: 'blazing',
+		version: 4,
+		website: 'https://svelte.dev'
+	};
 </script>
 
-<Nested answer={42} />
+<Nested answer={42} />  
+<!-- we are not using the answer prop, so it uses the default value -->
+<Nested />
+
+<!-- we can spread props, so the below... -->
+<PackageInfo
+	name={pkg.name}
+	speed={pkg.speed}
+    version={pkg.version}   
+	website={pkg.website}
+/>
+
+<!-- can be written easier as such -->
+<PackageInfo {...pkg} />
--- src/routes/props/Nested.svelte
+++ src/routes/props/Nested.svelte
@@ -1,5 +1,7 @@
 <script>
-	let answer;
+    // you can also simply "let answer" without assigning a value
+    // we are assigning to specifiy a default value
+	export let answer = "a mystery.";
 </script>
 
 <p>The answer is {answer}</p>
