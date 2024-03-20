d39007c9c3b7e0632e83f4f72c3e66450031e6c9
staswerno <staswerno@gmail.com>
2024-03-20 18:30:47 +0100

    add external api

--- src/routes/await-blocks/+page.svelte
+++ src/routes/await-blocks/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { getRandomNumber } from './utils.js';
 
 	let promise = getRandomNumber();
@@ -16,7 +16,7 @@ import { getRandomNumber } from './utils.js';
 {#await promise}
 	<p>...waiting</p>
 {:then number}
-	<p>the number is {number}</p>
+	<p>the number is {number[0]}</p>
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
@@ -27,7 +27,7 @@ meaning you don't need to worry about race conditions.
 -->
 
 <!--
-	If you know that your promise can't reject,
+If you know that your promise can't reject,
 you can omit the catch block.
 You can also omit the first block if you don't
 want to show anything until the promise resolves:
@@ -37,23 +37,4 @@ want to show anything until the promise resolves:
 {#await promise then number}
 	<p>The number is {number}</p>
 {/await}
--->
-
-<!--
-try adding an API endpoint when the time comes
-to test this properly
-use the following code as a baseline:
-
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/random-number', (req, res) => {
-    const randomNumber = Math.floor(Math.random() * 101); // generates a random number between 0 and 100
-    res.send(randomNumber.toString());
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
 -->
--- src/routes/await-blocks/utils.js
+++ src/routes/await-blocks/utils.js
@@ -1,10 +1,9 @@
 export async function getRandomNumber() {
     // Fetch a random number between 0 and 100
     // (with a delay, so that we can see it)
-    const res = await fetch('/random-number');
-
-    if (res.ok) {
-        return await res.text();
+    const response = await fetch('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new');
+    if (response.ok) {
+        return await response.text();
     } else {
         // Sometimes the API will fail!
         throw new Error('Request failed');
