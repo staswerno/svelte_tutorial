66ef2f3c54ccb1565a255406cce5bf1517cbe5c7
staswerno <staswerno@gmail.com>
2024-03-25 16:30:18 +0100

    add event modifiers

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -8,18 +8,33 @@
 </script>
 
 <div on:pointermove={handleMove}>
-  Function declared in the script:
-	The pointer is at {m.x} x {m.y}
-</div>
-
-<!--
+	the pointer is at {m.x} x {m.y}
+  <!--
 Here is the same function declared inline:
-
 <div on:pointermove={(e) => {m = {x: e.clientX, y: e.clientY}}}>
-  Function declared inline:
 	The pointer is at {m.x} x {m.y}
 </div>
 -->
+  <br /><br />
+  <h4>event modifiers</h4>
+  <p><code>once</code> modifier:</p>
+  <button on:click|once={() => alert('clicked')}>
+    click me
+  </button>
+  <p>other modifiers include:</p>
+  <ol>
+    <li><code>preventDefault</code> — calls <code>event.preventDefault()</code> before running the handler. Useful for client-side form handling, for example.</li>
+    <li><code>stopPropagation</code> — calls <code>event.stopPropagation()</code>, preventing the event reaching the next element</li>
+    <li><code>passive</code> — improves scrolling performance on touch/wheel events (Svelte will add it automatically where it's safe to do so)</li>
+    <li><code>nonpassive</code> — explicitly set <code>passive: false</code></li>
+    <li><code>capture</code> — fires the handler during the capture phase instead of the bubbling phase</li>
+    <li><code>once</code> — remove the handler after the first time it runs</li>
+    <li><code>self</code> — only trigger handler if event.target is the element itself</li>
+    <li><code>trusted</code> — only trigger handler if event.isTrusted is true, meaning the event was triggered by a user action rather than because some JavaScript called element.dispatchEvent(...)</li>
+  </ol>
+</div>
+
+<!-- You can chain modifiers together, e.g. on:click|once|capture={...}. -->
 
 <style>
 	div {
