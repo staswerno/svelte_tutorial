f0a0697d6317db8fae9f220337f9b221429253c3
staswerno <staswerno@gmail.com>
2024-03-25 14:09:55 +0100

    listening to DOM events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,21 +1,35 @@
 <script>
-  let name = 'world';
+	let m = { x: 0, y: 0 };
+
+	function handleMove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
 </script>
 
-<main>
-  <h1>Hello, {name}!</h1>
-</main>
+<div on:pointermove={handleMove}>
+  Function declared in the script:
+	The pointer is at {m.x} x {m.y}
+</div>
+
+<!--
+Here is the same function declared inline:
+
+<div on:pointermove={(e) => {m = {x: e.clientX, y: e.clientY}}}>
+  Function declared inline:
+	The pointer is at {m.x} x {m.y}
+</div>
+-->
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #333;
-    font-size: 2em;
-  }
+	div {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		padding: 1rem;
+    background-color: palegoldenrod;
+    font-family: Arial, Helvetica, sans-serif
+	}
 </style>
