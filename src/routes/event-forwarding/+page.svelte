b3be24d21d5b93f2f960608664c113456c7e5337
staswerno <staswerno@gmail.com>
2024-03-26 12:42:40 +0100

    add event forwarding example

--- src/routes/event-forwarding/+page.svelte
+++ src/routes/event-forwarding/+page.svelte
@@ -6,4 +6,8 @@
 	}
 </script>
 
+<!-- component events don't bubble. if you want to 
+listen to an event on some deeply nested component, 
+the intermediate components must forward the event -->
+
 <Outer on:message={handleMessage} />
--- src/routes/event-forwarding/Outer.svelte
+++ src/routes/event-forwarding/Outer.svelte
@@ -2,4 +2,25 @@
 	import Inner from './Inner.svelte';
 </script>
 
-<Inner />
+<!-- instead of writing another event dispatcher function
+svelte has shorthand to simply forward the event.
+so instead of: -->
+
+<!-- 
+    <script>
+        import Inner from './Inner.svelte';
+        import { createEventDispatcher } from 'svelte';
+
+        const dispatch = createEventDispatcher();
+
+        function forward(event) {
+            dispatch('message', event.detail);
+        }
+    </script>
+
+    <Inner on:message={forward}/> 
+-->
+
+<!-- we can have: -->
+
+<Inner on:message/>
