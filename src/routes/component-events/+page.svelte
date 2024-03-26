bda9d61b63890e6a9eee51f61258343346cc5506
staswerno <staswerno@gmail.com>
2024-03-26 11:34:49 +0100

    add component events example

--- src/routes/component-events/+page.svelte
+++ src/routes/component-events/+page.svelte
@@ -6,4 +6,11 @@
 	}
 </script>
 
-<Inner />
+<Inner on:message={handleMessage} />
+
+<!-- You can also try changing the event name 
+to something else. For instance, 
+change dispatch('message', {...}) to 
+dispatch('greet', {...}) in Inner.svelte 
+and change the attribute name from on:message to 
+on:greet in App.svelte. -->
--- src/routes/component-events/Inner.svelte
+++ src/routes/component-events/Inner.svelte
@@ -1,4 +1,13 @@
 <script>
+import { createEventDispatcher } from 'svelte';
+
+const dispatch = createEventDispatcher();
+
+// createEventDispatcher must be called when the component
+// is first instantiated — you can't do it later inside, 
+// for e.g., a setTimeout callback. This links dispatch to the 
+// component instance.
+
 	function sayHello() {
 		dispatch('message', {
 			text: 'Hello!'
